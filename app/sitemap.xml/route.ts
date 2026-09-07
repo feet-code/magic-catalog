4aac3b24d74534df28c268ab980f1e57732cee1c
feet-code <feetcodee@gmail.com>
2026-09-07 14:22:49 -0700

    Keep sitemap index valid by listing only sitemap documents

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,38 +1,21 @@
-import { catalogDirectoryCount } from "../../lib/catalog-directory";
 import { catalogProductCount } from "../../lib/catalog-sitemap";
 import { getSiteUrl } from "../../lib/runtime";
 
 export const dynamic = "force-dynamic";
-const DIRECTORY_PAGE_SIZE = 250;
 
 export async function GET() {
-  const [productCount, directoryCount] = await Promise.all([
-    catalogProductCount(),
-    catalogDirectoryCount(),
-  ]);
-  const productPages = Math.max(1, Math.ceil(productCount / 44_900));
-  const directoryPages = Math.max(1, Math.ceil(directoryCount / DIRECTORY_PAGE_SIZE));
+  const productCount = await catalogProductCount();
+  const pages = Math.max(1, Math.ceil(productCount / 44_900));
   const origin = getSiteUrl();
   const entries = Array.from(
-    { length: productPages },
+    { length: pages },
     (_, page) =>
       "<sitemap><loc>" +
       origin +
       "/sitemaps/" +
       page +
       "</loc></sitemap>",
-  ).concat([
-    "<sitemap><loc>" + origin + "/catalog</loc></sitemap>",
-    ...Array.from(
-      { length: Math.max(0, directoryPages - 1) },
-      (_, page) =>
-        "<sitemap><loc>" +
-        origin +
-        "/catalog/" +
-        (page + 1) +
-        "</loc></sitemap>",
-    ),
-  ]).join("");
+  ).join("");
   const xml =
     '<?xml version="1.0" encoding="UTF-8"?>' +
     '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
